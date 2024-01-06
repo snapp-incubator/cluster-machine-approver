745e4b8af1da428a98a5df4c26c9981f96eb06dc
Ehsan Aliakbar <e.aliakbar97@gmail.com>
2024-01-06 16:28:47 +0330

    Remove all Dependabot Changes

--- vendor/github.com/googleapis/gnostic/openapiv2/OpenAPIv2.go
+++ vendor/github.com/googleapis/gnostic/openapiv2/OpenAPIv2.go
@@ -7585,10 +7585,7 @@ func (m *NamedAny) ToRawInfo() *yaml.Node {
 		info.Content = append(info.Content, compiler.NewScalarNodeForString("name"))
 		info.Content = append(info.Content, compiler.NewScalarNodeForString(m.Name))
 	}
-	if m.Value != nil {
-		info.Content = append(info.Content, compiler.NewScalarNodeForString("value"))
-		info.Content = append(info.Content, m.Value.ToRawInfo())
-	}
+	// &{Name:value Type:Any StringEnumValues:[] MapType: Repeated:false Pattern: Implicit:false Description:Mapped value}
 	return info
 }
 
